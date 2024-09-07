576ef4edea7bcf183fe392999d81a10cf77431a8
Ben Swift <ben@benswift.me>
2024-09-07 21:13:55 +1000

    change to image2image

--- src/storytellers/__init__.py
+++ src/storytellers/__init__.py
@@ -1,14 +1,17 @@
 import storytellers.sdxl_turbo as sdxl
 import storytellers.camera as camera
+from diffusers.utils import load_image
 
 
 def main() -> int:
-    print("Hello from storytellers!")
-    image = sdxl.image_to_image(None, "a pear and an apple sitting happliy together")
-    print(image)
+    webcam_frame = camera.get_image()
+    webcam_frame.save("in.jpg")
+    webcam_frame = load_image("in.jpg")
+    # print("First 16 RGB values:", [tuple(map(int, rgb)) for rgb in first_16_rgb])
+    image = sdxl.image_to_image(
+        "a pear and an apple sitting happliy together", webcam_frame
+    )
 
-    image = camera.get_image()
     image.save("out.jpg")
-
     camera.cleanup()
     return 0
--- src/storytellers/sdxl_turbo.py
+++ src/storytellers/sdxl_turbo.py
@@ -1,15 +1,21 @@
-from diffusers import AutoPipelineForText2Image
 import torch
+from diffusers import AutoPipelineForImage2Image
 
-pipe = AutoPipelineForText2Image.from_pretrained(
+pipe = AutoPipelineForImage2Image.from_pretrained(
     "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
 )
 # hardcoded for my laptop for now, will make configurable later
 pipe.to("mps")
 
 
-def image_to_image(image, prompt):
+def image_to_image(prompt, input_image):
     output_image = pipe(
-        prompt=prompt, num_inference_steps=1, guidance_scale=0.0
+        prompt,
+        image=input_image,
+        # if num steps < 4, it errors out for some reason
+        num_inference_steps=4,
+        guidance_scale=0.0,
+        strength=0.3,
     ).images[0]
+
     return output_image
