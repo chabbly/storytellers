24232d5587db5d24151dcce9b56f5f2035280b2a
Ben Swift <ben@benswift.me>
2024-09-09 10:27:19 +1000

    add some init script to make it work

--- src/storytellers/__init__.py
+++ src/storytellers/__init__.py
@@ -4,11 +4,11 @@ from diffusers.utils import load_image
 
 
 def main() -> int:
-    # webcam_frame = camera.get_image()
-    # webcam_frame.save("in.jpg")
+    webcam_frame = camera.get_image()
+    webcam_frame.save("in.jpg")
 
-    webcam_frame = load_image("racoon.jpg")
-    image = sdxl.predict(webcam_frame, "an albino polar bear", 0.7, 2)
+    webcam_frame = load_image("in.jpg")
+    image = sdxl.predict(webcam_frame, "a scene from a Philip K. Dick novel", 0.7, 2)
 
     image.save("out.jpg")
     camera.cleanup()
