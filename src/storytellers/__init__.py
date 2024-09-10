8af0dddde9c3cd4f2a44b4e414f0b045d49bdb27
Ben Swift <ben@benswift.me>
2024-09-10 13:59:34 +1000

    add the loop video behaviour to main

--- src/storytellers/__init__.py
+++ src/storytellers/__init__.py
@@ -7,12 +7,18 @@ import storytellers.assets as assets
 
 
 def main() -> int:
+    frame_index = 1
     try:
         while True:
             webcam_frame = sdxl.resize_crop(camera.get_image())
-            video_frame = assets.read_image("nggyu", 4)
+            video_frame = assets.read_image("nggyu", frame_index)
+            if video_frame is None:
+                frame_index = 1
+                video_frame = assets.read_image("nggyu", frame_index)
+            else:
+                frame_index += 1
             image = bg_remover.combine(video_frame, webcam_frame)
-            # image = sdxl.predict(webcam_frame, "cubism meets pointillism", 0.7, 2)
+            # image = sdxl.predict(image, "cubism meets pointillism", 0.3, 2)
             viewer.show_image(image)
     except KeyboardInterrupt:
         pass
